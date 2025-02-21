cd3dfbaca414ba08e758ad568ec8889c1825af89
omdien <87163559+omdien@users.noreply.github.com>
2025-02-21 09:21:37 +0700

    commit pertama vite react bulma

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <h1 className="title center">Vite + React + Bulma CSS</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
